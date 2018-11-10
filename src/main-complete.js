fbfabcf59da789d4e92bc37299b09218a2d92f17
Aliyah Northington <alierin@Aliyahs-MBP.knology.net>
2018-11-10 13:12:32 -0600

    Create a variable of a text node from our buttons
    It will do this by taking the text of each button that we click,
    grabbing the child node, and grabbing the text value of that node.

--- src/main-complete.js
+++ src/main-complete.js
@@ -1,15 +1,15 @@
 const navButtons = document.querySelectorAll('#navbar ul li');
 
 function wrapper() {
-  const loopThrough = () => {
+	const loopThrough = () => {
+		let thisButtonText = this.childNodes[0].nodeValue;
 
+		for (let i = 0; i < thisButtonLength; i++) {
 
-    for (let i = 0; i < thisButtonLength; i++) {
+		}
 
-    }
-
-  };
-  loopThrough();
+	};
+	loopThrough();
 };
 
 for(let i = 0; i < navButtons.length; i++) {
